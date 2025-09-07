88c77dd52f9a16db02e9fafcd896a4e9dfb99de8
Craig Dennis <craigsdennis@gmail.com>
2025-09-07 13:30:41 -0700

    Adds reactions

--- worker/agents/presentation.ts
+++ worker/agents/presentation.ts
@@ -2,20 +2,42 @@ import { Agent } from "agents";
 import { unstable_callable as callable } from "agents";
 
 export type PresentationState = {
-    currentSlideIndex: number;
-}
+  currentSlideIndex: number;
+  availableReactions: string[];
+};
 
 export class PresentationAgent extends Agent<Env, PresentationState> {
-    initialState = {
-        currentSlideIndex: 0
-    }
+  initialState = {
+    currentSlideIndex: 0,
+    availableReactions: ["🧡", "😎", "🤷‍♂️"]
+  };
 
-    @callable()
-    async nextSlide() {
-        this.setState({
-            ...this.state,
-            currentSlideIndex: this.state.currentSlideIndex + 1
-        })
-    }
+  onStart() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    this.sql`CREATE TABLE IF NOT EXISTS slide_reactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            slide_index INTEGER NOT NULL,
+            reaction TEXT NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        );`;
+  }
 
+  @callable()
+  async nextSlide() {
+    this.setState({
+      ...this.state,
+      currentSlideIndex: this.state.currentSlideIndex + 1,
+    });
+  }
+
+  @callable()
+  async storeReaction(reaction: string) {
+    if (this.state.availableReactions.includes(reaction)) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        this
+          .sql`INSERT INTO slide_reactions (slide_index, reaction) VALUES (${this.state.currentSlideIndex}, ${reaction})`;
+    } else {
+        console.warn(`Reaction ${reaction} was not available`);
+    }
+  }
 }
